5848a910cd80c45fce776e624a0f93de20e0e256
Rohan <rohan.joshi@goodcreator.co>
2022-08-26 02:15:23 +0530

    feat: sms trigger

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:telephony/telephony.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -31,16 +37,46 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  Telephony telephony = Telephony.instance;
+  bool permissionsGranted = false;
+  String? deviceId;
+  String? phoneNo;
+  String? message;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  List<String> deviceIds = <String>['SP1A.210812.016'];
+
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  void sendSms(String? number, String? message) async {
+    if (number == null || message == null) {
+      return;
+    } else {
+      String currentId = await getDeviceId();
+      if (deviceIds.contains(currentId)) {
+        await telephony.sendSms(to: number, message: message);
+      }
+    }
+  }
+
+  //get deivce id
+  Future<String> getDeviceId() async {
+    //device info
+    await DeviceInfoPlugin().androidInfo.then((value) {
+      deviceId = value.id;
     });
+    debugPrint(deviceId);
+    return deviceId ?? '';
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    permissions();
+    getDeviceId();
+  }
+
+  void permissions() async {
+    permissionsGranted = await telephony.requestSmsPermissions ?? false;
   }
 
   @override
@@ -51,46 +87,84 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+
+    return StreamBuilder<QuerySnapshot>(
+        stream: firestore.collection('Message').snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          for (var element in snapshot.data?.docs ?? []) {
+            element.reference.delete();
+          }
+
+          final List? messagesString = snapshot.data?.docs.map((doc) {
+            return doc.data();
+          }).toList();
+
+          print(messagesString);
+
+          messagesString?.forEach((element) async {
+            sendSms(element["Phone"], element["Msg"]);
+          });
+
+          for (var element in snapshot.data?.docs ?? []) {
+            element.reference.delete();
+          }
+
+          return Scaffold(
+            appBar: AppBar(
+              // Here we take the value from the MyHomePage object that was created by
+              // the App.build method, and use it to set our appbar title.
+              title: Text(widget.title),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            body: Center(
+              // Center is a layout widget. It takes a single child and positions it
+              // in the middle of the parent.
+              child: Column(
+                // Column is also a layout widget. It takes a list of children and
+                // arranges them vertically. By default, it sizes itself to fit its
+                // children horizontally, and tries to be as tall as its parent.
+                // Invoke "debug painting" (press "p" in the console, choose the
+                // "Toggle Debug Paint" action from the Flutter Inspector in Android
+                // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+                // to see the wireframe for each widget.
+                //
+                // Column has various properties to control how it sizes itself and
+                // how it positions its children. Here we use mainAxisAlignment to
+                // center the children vertically; the main axis here is the vertical
+                // axis because Columns are vertical (the cross axis would be
+                // horizontal).
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    '$_counter',
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                sendSms(phoneNo, message);
+                snapshot.data?.docs.clear();
+              },
+              tooltip: 'Increment',
+              child: const Icon(Icons.add),
+            ), // This trailing comma makes auto-formatting nicer for build methods.
+          );
+        });
   }
 }
+
+final Stream<QuerySnapshot> collectionStream =
+    FirebaseFirestore.instance.collection('Messages').snapshots();
+
+//stream builder firestore
+
+
+
+
